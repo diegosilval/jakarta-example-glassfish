be7bc86dab7749535e42c339eb25b294011b7b9e
Diego Silva <diego.silva@apuntesdejava>
2025-07-12 00:06:31 -0500

    Initial project setup with Jakarta EE example
    Add Maven wrapper, project configuration, and basic Jakarta EE resources. Includes pom.xml, Maven wrapper scripts, REST endpoints, configuration files, and a sample index.html for a new Jakarta EE web application.

---
+++ src/main/java/com/apuntesdejava/expo/resources/ApplicationResource.java
@@ -0,0 +1,9 @@
+package com.apuntesdejava.expo.resources;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationResource extends Application {
+
+}
---
+++ src/main/java/com/apuntesdejava/expo/resources/HelloResource.java
@@ -0,0 +1,17 @@
+package com.apuntesdejava.expo.resources;
+
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
+
+@Path("hello")
+public class HelloResource {
+
+    @GET
+    public Response hello(@QueryParam("name") @DefaultValue("world") String name) {
+        var message = "Hello %s".formatted(name);
+        return Response.ok(message).build();
+    }
+}
